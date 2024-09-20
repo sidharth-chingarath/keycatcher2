9c6bed04895659afc1aef99dad8bee0b177630fe
sidharth-chingarath <sidharthholms.ccna+github@gmail.com>
2024-09-20 12:14:46 +0530

    refactor : changes in observer side

--- src/observer/decryption/AES_decryptor.py
+++ src/observer/decryption/AES_decryptor.py
@@ -3,26 +3,29 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 # from ..load_env.my_env import MyENV
 
-key=b'5\x1ey5\xd5\xfeG\xa3\x97\xa0\x1b,\xdb\xe9\xda\xd2'
 
-print("key", key)
+class AESDecription:
+    def __init__(self) -> None:
+        self.write_path="../observer_file_pool/decrypted_file.txt"
+        self.read_path="../observer_file_pool/decoded_binary_from_base64_binary.bin"
 
+    def decryption_method(self):
+        key=b'5\x1ey5\xd5\xfeG\xa3\x97\xa0\x1b,\xdb\xe9\xda\xd2'
+        print("key", key)
+        # Decrypt the ciphertext
+        with open(self.read_path, "rb") as file:
+            iv = file.read(16)
+            ciphertext = file.read()
 
+        # Create a new cipher object with the same key and IV
+        cipher = AES.new(key, AES.MODE_CBC, iv)
 
-# Decrypt the ciphertext
-with open("../observer_file_pool/decoded_binary_from_base64_binary.bin", "rb") as file:
-    iv = file.read(16)
-    ciphertext = file.read()
+        # Decrypt the ciphertext
+        decrypted_data = cipher.decrypt(ciphertext)
 
-# Create a new cipher object with the same key and IV
-cipher = AES.new(key, AES.MODE_CBC, iv)
+        # Unpad the decrypted data
+        unpadded_data = unpad(decrypted_data, AES.block_size)
 
-# Decrypt the ciphertext
-decrypted_data = cipher.decrypt(ciphertext)
-
-# Unpad the decrypted data
-unpadded_data = unpad(decrypted_data, AES.block_size)
-
-# Save the decrypted data to a file
-with open("../observer_file_pool/decrypted_file.txt", "wb") as file:
-    file.write(unpadded_data)
+        # Save the decrypted data to a file
+        with open(self.write_path, "wb") as file:
+            file.write(unpadded_data)
